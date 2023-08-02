c45e96d524b551147dab7a18a83904b46500e031
1572817043 <1572817043@qq.com>
2023-08-02 23:45:44 +0800

    Site updated: 2023-08-02 23:45:43

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+WIDGET = {
+    "CONFIG": {
+        "modules": "01234",
+        "background": "5",
+        "tmpColor": "45C9C9",
+        "tmpSize": "16",
+        "cityColor": "45C9C9",
+        "citySize": "16",
+        "aqiColor": "45C9C9",
+        "aqiSize": "16",
+        "weatherIconSize": "24",
+        "alertIconSize": "18",
+        "padding": "10px 10px 10px 10px",
+        "shadow": "0",
+        "language": "auto",
+        "fixed": "false",
+        "vertical": "top",
+        "horizontal": "left",
+        "key": "a2767c2e1dad415eb0aeb70c65163a50"
+    }
+}
